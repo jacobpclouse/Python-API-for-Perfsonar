5c06fcbbd0115ea9c6ef51b94abff3db90a845d2
Jacob <clouse.jp@gmail.com>
2022-04-04 17:29:28 -0400

    added response code and print json

--- python_API_PerfSONAR.py
+++ python_API_PerfSONAR.py
@@ -1,1 +1,9 @@
 # https://www.dataquest.io/blog/python-api-tutorial/
+
+import requests
+
+
+response = requests.get("https://192.168.50.190/esmond/perfsonar/archive/")
+print(response.status_code)
+
+print(response.json())
