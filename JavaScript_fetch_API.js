193e466914df552040212fbd1da29dbec2c3dd12
jc366512 <jclouse@albany.edu>
2022-04-05 12:27:29 -0400

    added links to promises and async await tutorials -- learned to stringify the json before sending out, just need to send GET request for now

--- JavaScript_fetch_API.js
+++ JavaScript_fetch_API.js
@@ -11,6 +11,37 @@
 // Error I ran into: 
 // https://search.brave.com/search?q=VM20%3A1+Uncaught+(in+promise)+TypeError%3A+Failed+to+fetch+at+%3Canonymous%3E%3A1%3A1&source=desktop
 
-fetch('https://regres.in/api/users')
-    .then(res => res.json())
+// ----- Useful videos and links -----
+// Learn Promises in JS
+// https://www.youtube.com/watch?v=DHvZLI7Db8E&t=0s
+
+// Async Await Tutorial
+// https://www.youtube.com/watch?v=V_Kr9OSfDeU&t=0s
+
+
+// -- OLD --
+// fetch('https://regres.in/api/users')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+
+
+fetch('https://regres.in/api/users', {
+    // Defaults to GET, can remove method, headers and body labels here if you just want to use GET
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'User 1'
+    })
+})
+    .then(res => {
+        // if (res.ok){
+        //     console.log('SUCCESS!!')
+        // } else {
+        //     console.log('Response was not Successfull...')
+        // }
+        // res.json()
+        return res.json()
+    })
     .then(data => console.log(data))
