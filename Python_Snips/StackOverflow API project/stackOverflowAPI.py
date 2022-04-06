243f787c6c6688c1b46620f7ce45797127a9f48c
jc366512 <jclouse@albany.edu>
2022-04-06 12:18:48 -0400

    ran test response to stackoverflow api, got 200 response

--- Python_Snips/StackOverflow API project/stackOverflowAPI.py
+++ Python_Snips/StackOverflow API project/stackOverflowAPI.py
@@ -1,4 +1,9 @@
 # video tutorial is located at: https://www.youtube.com/watch?v=qbLc5a9jdXo&t=1283s
-
 # api is located at: https://api.stackexchange.com/
 
+import requests
+import json
+
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+
+print(response)
