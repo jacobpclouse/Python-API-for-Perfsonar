a51eb44b44447acd40219b4f2cb1bc96c054bdce
jc366512 <jclouse@albany.edu>
2022-04-07 12:34:17 -0400

    added parameters to api, working on returning them with self

--- Python_Snips/StackOverflow API project/myAPI/application.py
+++ Python_Snips/StackOverflow API project/myAPI/application.py
@@ -1,7 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
 
+    def __repr__(self):
+        return f"{self.na"
+
+# https://youtu.be/qbLc5a9jdXo?t=2050
+
+# routes
+@app.route('/')
 def index():
-    return 'Hello!'
+    return 'Welcome to CyberSpace!'
+
+@app.route('/drinks')
+def get_drinks():
+    return {"drinks": "Drink data for right now!"}
