942a3d2d0fe6f7f62a0aeef8beef2997896dcb31
jc366512 <jclouse@albany.edu>
2022-04-12 12:11:33 -0400

    added comment with json format to test file

--- My Api Scripts/jsonTest.py
+++ My Api Scripts/jsonTest.py
@@ -2,4 +2,22 @@
 
 import json
 
-data = l
+people_string = '''
+
+{
+    "people": [
+        {
+            "name": "John Smith",
+            "phone": "615-555-7164",
+            "emails": ["johnsmith@bogusemail.com", "john.smith@work-place.com"],
+            "has_license": false
+        },
+        {
+            "name": "Jane Doe",
+            "phone": "560-555-5153",
+            "emails": null,
+            "has_license": true
+        }
+    ]
+}
+'''
