6568e7d79b316be210c382b05fc0e637600f53f8
jc366512 <jclouse@albany.edu>
2022-04-16 13:54:15 -0400

    added function that adds current date time to each output json

--- jsonTest Perfsonar API - V2.py
+++ jsonTest Perfsonar API - V2.py
@@ -3,9 +3,38 @@
 # working with objects in python: https://www.youtube.com/watch?v=Uh2ebFW8OYM
 # graphing data in python: https://www.geeksforgeeks.org/graph-plotting-in-python-set-1/
 
+
 ''' Javascript Object notation'''
+## THIS is how you grab data from the api, format it as a json and then output it with the correct info
+
 import json
 from urllib.request import urlopen
+import datetime
+# ----------
+# Variables:
+# ----------
+count = 0
+
+
+# ----------
+# Functions:
+# ----------
+
+# --- Defang date time ---
+def defang_datetime():
+    current_datetime = f'_{datetime.datetime.now()}'
+
+    current_datetime = current_datetime.replace(":","_")
+    current_datetime = current_datetime.replace(".","-")
+    current_datetime = current_datetime.replace(" ","_")
+    
+    return current_datetime
+
+'''
+MAIN
+'''
+# Grab current date & time from function & store in variable
+use_this_datetime = defang_datetime()
 
 # ------------------------------------------------
 # 192.168.50.190
@@ -34,15 +63,18 @@ for item in data:
     print('-=-=-=-=-=-=-=-=-=-=-=-')
     print(' ')
 
+    # incriment count
+    count = count + 1
+
 
 # create and write out server data 
 
-with open('My_Perfsonar_Server_jake_sorted_2.json', 'w') as z:
+with open(f'My_Perfsonar_Server_jake_sorted{use_this_datetime}.json', 'w') as z:
     json.dump(data, z, indent=2,sort_keys=True)
 ''''''
 
-## THIS is how you grab data from the api, format it as a json and then output it with the correct info
-
+# output count of all the total originating nodes
+print(f"Total node count = {count}")
 
 ## NOTE: remember to add the date function to dynamically name json outputs
 
