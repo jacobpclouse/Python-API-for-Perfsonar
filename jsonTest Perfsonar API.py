a78518ec412a3976dfdeb95efb976645a7f6f4e8
jc366512 <jclouse@albany.edu>
2022-04-16 12:05:38 -0400

    wrote new script that successfully queries my server api for info and outpus it into a formatted json file

---
+++ jsonTest Perfsonar API.py
@@ -0,0 +1,31 @@
+# https://www.youtube.com/watch?v=9N6a-VLBa2I
+# encoder for json in python docs: https://docs.python.org/3/library/json.html
+# working with objects in python: https://www.youtube.com/watch?v=Uh2ebFW8OYM
+
+''' Javascript Object notation'''
+import json
+from urllib.request import urlopen
+
+# with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json") as response:
+#     source = response.read()
+
+with urlopen("http://192.168.50.190/esmond/perfsonar/archive/") as response:
+    source = response.read()
+
+data = json.loads(source)
+
+print(json.dumps(data, indent=2))
+
+with open('My_Perfsonar_Server_jake_sorted.json', 'w') as z:
+    json.dump(data, z, indent=2,sort_keys=True)
+
+## THIS is how you grab data from the api, format it as a json and then output it with the correct info
+'''   
+outputSource = source.decode("utf-8")
+
+with open('D_&_D_jake.json', 'w') as z:
+    json.dump(outputSource, z, indent=2)
+    ''' 
+
+# 192.168.50.190
+# https://192.168.50.190/esmond/perfsonar/archive/
