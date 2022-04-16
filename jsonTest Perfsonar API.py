49918de54e92495a194b578f33c97a2cac27edae
jc366512 <jclouse@albany.edu>
2022-04-16 12:20:17 -0400

    formatted jsonTest Perfsonar API.py and documented every current step with comments

--- jsonTest Perfsonar API.py
+++ jsonTest Perfsonar API.py
@@ -1,31 +1,33 @@
-# https://www.youtube.com/watch?v=9N6a-VLBa2I
+# video this is based off of: https://www.youtube.com/watch?v=9N6a-VLBa2I
 # encoder for json in python docs: https://docs.python.org/3/library/json.html
 # working with objects in python: https://www.youtube.com/watch?v=Uh2ebFW8OYM
+# graphing data in python: https://www.geeksforgeeks.org/graph-plotting-in-python-set-1/
 
 ''' Javascript Object notation'''
 import json
 from urllib.request import urlopen
 
-# with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json") as response:
-#     source = response.read()
-
+# ------------------------------------------------
+# 192.168.50.190
+# https://192.168.50.190/esmond/perfsonar/archive/
+# ------------------------------------------------
+# read data from local ip of perfsonar server REST API -> store into source variable
 with urlopen("http://192.168.50.190/esmond/perfsonar/archive/") as response:
     source = response.read()
 
+# take source data, load as json and move to data var
 data = json.loads(source)
 
+# add formatting
 print(json.dumps(data, indent=2))
 
+# create and write out server data
 with open('My_Perfsonar_Server_jake_sorted.json', 'w') as z:
     json.dump(data, z, indent=2,sort_keys=True)
 
 ## THIS is how you grab data from the api, format it as a json and then output it with the correct info
-'''   
-outputSource = source.decode("utf-8")
 
-with open('D_&_D_jake.json', 'w') as z:
-    json.dump(outputSource, z, indent=2)
-    ''' 
 
-# 192.168.50.190
-# https://192.168.50.190/esmond/perfsonar/archive/
+## NOTE: remember to add the date function to dynamically name json outputs
+
+
